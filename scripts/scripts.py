f8a2106c64d2353f5afea3d6586c8a2971219a09
Dorjey Sherpa <dorjeys3@gmail.com>
2020-12-11 16:51:49 -0500

    added functions for cleaning text

--- scripts/scripts.py
+++ scripts/scripts.py
@@ -1,6 +1,7 @@
 # all fucntions for the projects live here
 
 
+# for processing noisy contineous values
 def convertor(x):
     '''Converts string to a list and returns index 0'''
     #used to clean raw price column and num_rating column
@@ -18,4 +19,25 @@ def remove_dollar(x):
     x = list(x.split("$"))
     return x[1]
 
-cleaning_df["price"]= cleaning_df["price"].apply(remove_dollar)
+
+# for processing string values
+def clean_text(text):
+    text.replace("\\n"," ")
+    text =  ' '.join(re.sub("([^A-Za-z \t])"," ",text).split())
+    return text.lower()
+
+def tokenize(text):
+    return nltk.word_tokenize(text)
+
+def remove_stopwords(text):
+    return [word for word in text if word not in stop_words]
+
+def lemmatize_text(text):
+    return [lemmatizer.lemmatize(word) for word in text]
+
+def fdist(df, column):
+    corpus = " ".join([" ".join(text) for text in df[column].to_list()])
+    corpus = tokenize(corpus)
+    fdist=FreqDist(corpus)
+    print(f"Number of words in corpus: {len(fdist)}")
+    fdist.plot(20);
