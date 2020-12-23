c67eaf3db5bf5dbd450aeeabf0b874b6985b0a9e
Dorjey Sherpa <dorjeys3@gmail.com>
2020-12-23 16:51:02 -0500

    changed clean_text function

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,16 +1,23 @@
+import pandas as pd
+import numpy as np
 import re
 import nltk
 from nltk.probability import FreqDist
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
+from sklearn.preprocessing import OneHotEncoder
+
 
 lemmatizer = nltk.stem.WordNetLemmatizer()
+stop_words=list(set(stopwords.words("english")))
 # for processing string values
+
 def clean_text(text):
     text.replace("\\n"," ")
-    text =  ' '.join(re.sub("([^A-Za-z \t])"," ",text).split())
-    return text.lower()
+    text =  ' '.join(re.sub("([^A-Za-z0-9!.])"," ",text).split())
+    return text
+
 
 def tokenize(text):
     return nltk.word_tokenize(text)
@@ -21,3 +28,15 @@ def remove_stopwords(text):
 
 def lemmatize_text(text):
     return [lemmatizer.lemmatize(word) for word in text]
+
+
+def list_to_str(text):
+    return ", ".join(text)
+
+
+def coded_encoder(df, column):
+    encoder = OneHotEncoder(handle_unknown="error", drop="first" )
+    coded_cat=pd.DataFrame(encoder.fit_transform(df[[column]]).toarray())
+    coded_cat.rename(columns={0:"girls'", 1:"men's", 2:"women's"},inplace=True)
+    df.drop(columns = column, inplace=True)
+    return coded_cat
